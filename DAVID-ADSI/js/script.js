108edfdd360e6283f7334a72cd60faa6eade787f
Aleja0711 <75532396+Aleja0711@users.noreply.github.com>
2021-08-24 21:54:52 -0500

    Delete DAVID-ADSI/js directory

--- DAVID-ADSI/js/script.js
+++
@@ -1,4 +0,0 @@
-var base=prompt("Ingrese el valor de la base:")
-var altura=prompt("Ingrese el valor de la altura:")
-var area= ( base*altura)/2;
-document.writeln("El area del triangulo es:"+area);
